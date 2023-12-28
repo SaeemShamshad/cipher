f98efbf9e39977e694262102571fd754b44e506f
Ohm_mega <saeemshamshad@gmail.com>
2023-12-28 16:28:22 +0530

    Update for Space Atbash cipher.py

--- Atbash cipher.py
+++ Atbash cipher.py
@@ -1,7 +1,9 @@
 message = input("enter your message to be ciphered:")
- 
 for x in message :
-    unicode = ord(x)
-    n_factor = unicode - 97
-    new_unicode = unicode + (25 - n_factor*2)
-    print(chr(new_unicode))    
+    if x != " ":
+        unicode = ord(x)
+        n_factor = unicode - 97
+        new_unicode = unicode + (25 - n_factor*2)
+        print(chr(new_unicode))    
+    elif x == " ":
+        print(" ")
